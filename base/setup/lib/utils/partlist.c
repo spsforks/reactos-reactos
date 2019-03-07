815bb549e013bfae8d34df2ef3b696ff3da63079
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2019-03-07 02:30:08 +0100

    [SETUP:REACTOS] Partitioning support
    - Now I can reinsert newly-created partitions, or updating the
      unpartitioned space when deleting partitions, in correct order,
      not touching the PartInfo data associated with the tree items
      that aren't related to the items being modified.
    
    - Reliably delete data associated to tree-list items via the
      TVN_DELETEITEM notification.
      This allows also to simplify the cleanup function; keeping simple
      the code that deletes items when deleting partitions, etc.
    
    - Make the PrintPartitionData() function call itself recursively on
      extended partitions to display the sub-list of logical partitions.
      This also keeps the creation-partition code simple.
    
    - Handle the partition size in MB the same way as in USETUP
    
    - Correctly reset the progressbar position to zero when starting file copy.
    - Add missing file copy error handler in FileCopyCallback -- Just copying what USETUP does.

--- base/setup/lib/utils/partlist.c
+++ base/setup/lib/utils/partlist.c
@@ -2773,7 +2773,6 @@ UpdateDiskLayout(
  *
  * @return  The adjacent unpartitioned region, if it exists, or NULL.
  **/
-static
 PPARTENTRY
 GetAdjUnpartitionedEntry(
     _In_ PPARTENTRY PartEntry,
